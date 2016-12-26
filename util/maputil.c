aa0d09bff71287587802e0ad04eeaf9cb7324438
Serigne Amsatou Seye <saseye@emi.u-bordeaux1.fr>
2016-12-26 04:10:50 +0100

    Ajout du fichier maputil.c

---
+++ util/maputil.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+unsigned getwidth(char* filename){
+
+}
+
+unsigned getheight(char* filename){
+
+}
+
+unsigned getobjects(char* filename){
+
+}
+
+unsigned getinfo(char* filename){
+
+}
+
+void setwidth(char* filename){
+
+}
+
+void setheight(char* filename){
+	
+}
+
+
+
+
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
