14d7267f87e58fb4f1ead9f7dbebf17931b2c4c5
tlevolle <thomaslev94@gmail.com>
2017-01-02 16:36:28 +0100

    Implémentation de la fonction timer_init

--- src/tempo.c
+++ src/tempo.c
@@ -24,10 +24,42 @@ static unsigned long get_time (void)
 
 #ifdef PADAWAN
 
-// timer_init returns 1 if timers are fully implemented, 0 otherwise
-int timer_init (void)
-{
+void foe (int param) { // TODO : Complete and rename it later
+  printf("sdl_push_event(%p) appelee au temps %ld\n", param, get_time()); // NOT READY
   // TODO
+}
+
+void *handler (*void arg) {
+  struct sigaction sa;
+  sa.sa_handler = foe;// TODO : Rename it later
+  sigemptyset(&sa.sa_mask);
+  sa.sa_flags = 0;
+
+  sigset_t mask;
+  sigset_t empty_mask;
+
+  sigemptyset(&mask);
+  sigemptyset(&empty_mask);
+  sigaction(SIGALARM, &sa, NULL);
+
+  while(1) {
+    sigsuspend(&empty_mask);
+  }
+}  
+  
+// timer_init returns 1 if timers are fully implemented, 0 otherwise
+int timer_init (void) {
+  sigset_t mask;
+  sigemptyset(&mask);
+  sigaddset(&mask, SIGALARM);
+  sigprocmask(SIG_BLOCK, &mask, NULL);
+
+  pthread_t pid = (pthread_t)NULL; 
+  if (pthread_create(&pid, NULL, handler, (void *)pid) != 0) {
+    perror("The thread creation failed !\n");
+    exit(EXIT_FAILURE);
+  }
+  
   return 0; // Implementation not ready
 }
 
